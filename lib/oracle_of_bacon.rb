adeb45113206836461fb01c253d716a5fd65da31
Miriam Lee <mimibaari+githubmimiAsusIII@gmail.com>
2016-10-01 19:45:18 -0500

    Week 1 > Oracle of Bacon HW
    Your submission was recorded at 2016-10-02 00:54:33 +0000 : submission is on time.
    Score: 29/29
    
    Finished in 0.13469 seconds (files took 0.87336 seconds to load)
    29 examples, 0 failures

--- lib/oracle_of_bacon.rb
+++ lib/oracle_of_bacon.rb
@@ -20,11 +20,16 @@ class OracleOfBacon
   validate :from_does_not_equal_to
 
   def from_does_not_equal_to
-    # YOUR CODE HERE
+    if @to == @from 
+      errors.add(:from, "from can't be the same as to")
+    end
   end
 
   def initialize(api_key='')
     # your code here
+    @api_key = api_key
+    @from = "Kevin Bacon"
+    @to = "Kevin Bacon"
   end
 
   def find_connections
@@ -37,13 +42,20 @@ class OracleOfBacon
       # convert all of these into a generic OracleOfBacon::NetworkError,
       #  but keep the original error message
       # your code here
+      raise NetworkError, e
     end
     # your code here: create the OracleOfBacon::Response object
+    res = Response.new(xml)
+    # if res.type == :error
+
   end
 
   def make_uri_from_arguments
     # your code here: set the @uri attribute to properly-escaped URI
     #   constructed from the @from, @to, @api_key arguments
+
+    @uri = "http://oracleofbacon\.org/p=#{CGI.escape(@api_key)}&b=#{CGI.escape(@to)}&a=#{CGI.escape(@from)}"
+    puts @uri
   end
       
   class Response
@@ -59,11 +71,51 @@ class OracleOfBacon
     def parse_response
       if ! @doc.xpath('/error').empty?
         parse_error_response
+
       # your code here: 'elsif' clauses to handle other responses
       # for responses not matching the 3 basic types, the Response
       # object should have type 'unknown' and data 'unknown response'         
+      elsif ! @doc.xpath('//link').empty? 
+        parse_graph_response
+        
+      elsif ! @doc.xpath('//spellcheck').empty? 
+        parse_spellcheck_response
+      else
+        @type = :unknown
+        @data = 'unknown response type'
       end
     end
+
+    def parse_graph_response
+            # puts @doc.at_xpath('//link')
+        @type = :graph
+
+        # puts "count"
+        # puts @doc.at_xpath('link').element_children.count
+                        # puts @doc.xpath('//actor').length
+        # puts @doc.at_xpath('link').element_children
+        @data=Array.new
+        @doc.at_xpath('//link').element_children.all?{|c|
+            # puts c.to_s
+            # puts "c #{c.text}"
+            # if c.element?
+              # puts "c elem #{c.text}"
+              @data<<c.text
+            # end
+          }
+    end
+
+    def parse_spellcheck_response
+      #TODO for each  a simple array of all the possible spelling variants   
+      @type = :spellcheck
+      @data = Array.new
+      @doc.at_xpath('//spellcheck').element_children.all?{|c|
+        @data << c.text
+        }
+
+    end
+
+
     def parse_error_response
       @type = :error
       @data = 'Unauthorized access'
